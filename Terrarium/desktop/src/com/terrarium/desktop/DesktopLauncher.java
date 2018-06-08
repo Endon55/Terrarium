d13337eb5af82d797b3315652968835340d2ef54
Anthony Cosenza <anthony@anthonycosenza.com>
2018-06-08 12:51:48 -0600

    Project generation.

---
+++ Terrarium/desktop/src/com/terrarium/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.terrarium.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.terrarium.Terrarium;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Terrarium(), config);
+	}
+}
